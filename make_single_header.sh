c3e98257174e14e5441a9b6ec87b82846710d9e7
James Montgomery <James.Brian.Montgomery@gmail.com>
2025-10-20 00:02:16 -0400

    Update single header generation with sha256 header.

--- make_single_header.sh
+++ make_single_header.sh
@@ -2,10 +2,10 @@
 # Run this script to generate a single-header include for Ollama.hpp that combines the header-only libraries in ./include
 
 # Remove the includes to other libraries in ollama.hpp
-sed -e "47d" -e "54d" -e "62d" "include/ollama.hpp" > "no_includes_ollama.hpp"
+sed -e "47d" -e "54d" -e "62d" -e "69d" "include/ollama.hpp" > "no_includes_ollama.hpp"
 
 # Combine the headers into one file
-{ cat "include/json.hpp"; echo -e "\n\n"; cat "include/httplib.h"; echo -e "\n\n"; cat "include/Base64.h"; echo -e "\n\n"; cat "no_includes_ollama.hpp"; } > "singleheader/ollama.hpp"
+{ cat "include/json.hpp"; echo -e "\n\n"; cat "include/httplib.h"; echo -e "\n\n"; cat "include/Base64.h"; echo -e "\n\n"; cat "include/sha256.hpp"; echo -e "\n\n"; cat "no_includes_ollama.hpp"; } > "singleheader/ollama.hpp"
 
 # Clean up
 rm "no_includes_ollama.hpp"
